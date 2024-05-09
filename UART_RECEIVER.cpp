38cd6bb6024e44b0a1eb88db3614fd1865a38811
harsha vardhan <62476244+PEDDOLLA-HARSHAVARDHAN-REDDY@users.noreply.github.com>
2024-05-09 21:32:05 +0530

    Update UART_RECEIVER.cpp

--- UART_RECEIVER.cpp
+++ UART_RECEIVER.cpp
@@ -4,40 +4,6 @@
 #define BAUD 9600
 #define MYUBRR (FOSC/16/BAUD-1)
 
-volatile uint8_t *opData = (volatile uint8_t*)0x31,*opCntrl = (volatile uint8_t*)0x108;
-
-void del(){
-  volatile int i=0;
-  for(i=0;i<100;i++);
-}
-
-void writeCntrl(unsigned char cmd){
-  *opCntrl = (0x00);// enable low, RS is low
-  *opData = cmd;
-  del();
-  *opCntrl = (*opCntrl | (0x02)); //enable is high
-  del();
-  *opCntrl = (0x00);// enable low, RS low
-  del();
-}
-
-void writeData(unsigned char data){
-  *opCntrl = (0x01);// enable low, RS is high
-  *opData = data;
-  del();
-  *opCntrl = (*opCntrl | (0x02)); //enable is high
-  del();
-  *opCntrl = (0x01); // enable low
-  del();
-  *opCntrl = (0x00);// enable low, RS low
-  del();
-}
-
-void setUpDisplay(){
-  writeCntrl(0x01); // clear display
-  writeCntrl(0x0F); // display on, cursor, cursor blink
-  writeCntrl(0x06); // move cursor right
-}
 
 void USART_Init(unsigned int ubrr){
 /* Set baud rate */
@@ -62,22 +28,11 @@ unsigned char lp() {
   return USART_Receive();
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  volatile uint8_t *ddrData = (volatile uint8_t*)0x30,*ddrCntrl = (volatile uint8_t*)0x107;
-  *ddrData = 0xFF;
-  *ddrCntrl = 0xFF;
-
-  //setUpDisplay();
-  USART_Init ( MYUBRR );
-}
-
 int main() {
   // put your setup code here, to run once:
-  setup();
+  USART_Init ( MYUBRR );
   while(1){
-    char temp=lp();
-    //writeData(temp);
+    lp();
   }
   return 0;
 }
